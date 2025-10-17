1af1decb4da2437a29231e4315591697f7fb4a3b
silvaOrdep <pedro.o.silva975@gmail.com>
2025-10-17 13:58:07 -0300

    fix: message structure change

--- src/main/java/com/ordep/emailservice/dto/ContactRequest.java
+++ src/main/java/com/ordep/emailservice/dto/ContactRequest.java
@@ -3,5 +3,5 @@ package com.ordep.emailservice.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record ContactRequest(@Email String sender, @NotBlank String subject, @NotBlank String message) {
+public record ContactRequest(@Email String recipient, @NotBlank String subject, @NotBlank String message) {
 }
--- src/main/java/com/ordep/emailservice/service/EmailService.java
+++ src/main/java/com/ordep/emailservice/service/EmailService.java
@@ -1,17 +1,15 @@
 package com.ordep.emailservice.service;
 
 import com.ordep.emailservice.dto.ContactRequest;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
     private final JavaMailSender mailSender;
-    @Value("${spring.mail.username}")
-    private String recipient;
 
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
@@ -19,14 +17,17 @@ public class EmailService {
 
     public String sendContactRequest(ContactRequest contactRequest) {
         try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setFrom(contactRequest.sender());
-            mailMessage.setSubject(contactRequest.subject());
-            mailMessage.setTo(recipient);
+            MimeMessagePreparator mailMessage = mimeMessage -> {
+                MimeMessageHelper message = new MimeMessageHelper(
+                        mimeMessage, true, "UTF-8");
+                message.setTo(contactRequest.recipient());
+                message.setSubject(contactRequest.subject());
+                message.setText(contactRequest.message());
+            };
             mailSender.send(mailMessage);
             return "Contact request email sent successfully";
         } catch (Exception e) {
-            return "An error occurred while trying to send a contact request: "+e.getMessage();
+            return "An error occurred while trying to send a contact request - "+e.getMessage();
         }
     }
 
